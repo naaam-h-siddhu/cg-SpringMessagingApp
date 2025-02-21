8c6525d7fa0e214db3636060709273bf26f29269
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-21 20:17:39 +0530

    ✨[Sudhakar] feat: Add REST API endpoint to greet user with name as query parameter

--- src/main/java/org/bridgelabz/siddhu/springmessagingapp/controller/HelloController.java
+++ src/main/java/org/bridgelabz/siddhu/springmessagingapp/controller/HelloController.java
@@ -2,6 +2,7 @@ package org.bridgelabz.siddhu.springmessagingapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,9 @@ public class HelloController {
     @GetMapping("hello")
     public String printer(){
         return "Hello !!!";
+    }
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(name = "name", defaultValue = "Guest") String name){
+        return "Hello "+name+" from BridgeLabz";
     }
 }
