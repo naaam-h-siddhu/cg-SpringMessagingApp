6b826f4c8e3cfe1b4b80ee1285dbfc0fa3ce5298
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-21 21:22:09 +0530

    ✨[Sudhakar] feat: Add path variabel to /hello endpoint for personlized greeting

--- src/main/java/org/bridgelabz/siddhu/springmessagingapp/controller/HelloController.java
+++ src/main/java/org/bridgelabz/siddhu/springmessagingapp/controller/HelloController.java
@@ -2,6 +2,7 @@ package org.bridgelabz.siddhu.springmessagingapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,16 @@ public class HelloController {
     public String printer(){
         return "Hello !!!";
     }
+
+    // Passing name as query parameter
     @GetMapping("/hello/query")
     public String sayHello(@RequestParam(name = "name", defaultValue = "Guest") String name){
         return "Hello "+name+" from BridgeLabz";
     }
+    //passing name as path variable
+    @GetMapping("hello/{name}")
+    public String sayHelloUsingPathVariable(@PathVariable String name){
+        return "Hello "+name+" from BridgeLabz";
+    }
+
 }
