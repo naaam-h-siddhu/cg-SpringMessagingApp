31f7babea4302cec6554857512b4b507da67d743
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-21 16:48:53 +0530

    🎉 Initial commit

---
+++ src/main/java/org/bridgelabz/siddhu/springmessagingapp/CgSpringMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package org.bridgelabz.siddhu.springmessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgSpringMessagingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgSpringMessagingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bridgelabz/siddhu/springmessagingapp/CgSpringMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bridgelabz.siddhu.springmessagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgSpringMessagingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
